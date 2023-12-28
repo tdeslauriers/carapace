e86b72eb38e239ec346920a847ecfccf8429aa26
tdeslauriers <thomas.deslauriers@gmail.com>
2023-12-28 10:02:10 -0600

    updated crud with certs

--- data/crud_test.go
+++ data/crud_test.go
@@ -1,28 +1,66 @@
 package data
 
 import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/tdeslauriers/carapace/certs"
 	"github.com/tdeslauriers/carapace/connect"
 )
 
+// env vars in db_connect_test.go
+
 func TestCrud(t *testing.T) {
 
+	// setup
+	// gen client certs
+	// need to use ca installed in maria as rootCA
+	leafClient := certs.CertFields{
+		CertName:     "db-client",
+		Organisation: []string{"Rebel Alliance"},
+		CommonName:   "localhost",
+		San:          []string{"localhost"},
+		SanIps:       []net.IP{net.ParseIP("127.0.0.1")},
+		Role:         certs.Client,
+		CaCertName:   "db-ca",
+	}
+	leafClient.GenerateEcdsaCert()
+
+	// read in db-ca-cert.pem, new db-client .pems to env vars
+	// need to use ca that signed maria's tls leaf certs
+	var envVars [][]string
+	envVars = append(envVars, []string{DbServerCaCert, fmt.Sprintf("%s-cert.pem", "db-ca")})
+	envVars = append(envVars, []string{DbClientCert, fmt.Sprintf("%s-cert.pem", leafClient.CertName)})
+	envVars = append(envVars, []string{DbClientKey, fmt.Sprintf("%s-key.pem", leafClient.CertName)})
+
+	// loop thru setting env
+	for _, v := range envVars {
+
+		fileData, _ := os.ReadFile(v[1])
+		encodedData := base64.StdEncoding.EncodeToString(fileData)
+		if err := os.Setenv(v[0], encodedData); err != nil {
+			log.Fatalf("Unable to load env var: %s", v[0])
+		}
+	}
+
 	dbPki := connect.Pki{
-		CertFile: os.Getenv("CLIENT_CERT"),
-		KeyFile:  os.Getenv("CLIENT_KEY"),
-		CaFiles:  []string{os.Getenv("DB_CA_CERT")},
+		CertFile: os.Getenv(DbClientCert),
+		KeyFile:  os.Getenv(DbClientKey),
+		CaFiles:  []string{os.Getenv(DbServerCaCert)},
 	}
 	clientConfig := connect.ClientConfig{Config: &dbPki}
 
 	url := DbUrl{
-		Username: os.Getenv("CARAPACE_MARIADB_USERNAME"),
-		Password: os.Getenv("CARAPACE_MARIADB_PASSWORD"),
-		Addr:     os.Getenv("CARAPACE_MARIADB_URL"),
-		Name:     os.Getenv("CARAPACE_MARIADB_NAME"),
+		Username: os.Getenv(MariaDbUsername),
+		Password: os.Getenv(MariaDbPassword),
+		Addr:     os.Getenv(MariaDbUrl),
+		Name:     os.Getenv(MariaDbName),
 	}
 
 	dbConnector := &SqlDbConnector{
@@ -32,16 +70,19 @@ func TestCrud(t *testing.T) {
 
 	id, _ := uuid.NewRandom()
 	sessionToken, _ := uuid.NewRandom()
+	csrf, _ := uuid.NewRandom()
 
 	// anonymous struct to avoid circular imports in testing.
 	s := struct {
 		Uuid         string `db:"uuid"`
 		SessionToken string `db:"session_token"`
+		CsrfToken    string `db:"csrf_token"`
 		CreatedAt    string `db:"created_at"`
 		ExpiresAt    string `db:"expires_at"`
 	}{
 		Uuid:         id.String(),
 		SessionToken: sessionToken.String(),
+		CsrfToken:    csrf.String(),
 		CreatedAt:    time.Now().Format("2006-01-02 15:04:05"),
 		ExpiresAt:    time.Now().Add(time.Minute * 15).Format("2006-01-02 15:04:05"),
 	}
@@ -55,6 +96,7 @@ func TestCrud(t *testing.T) {
 	var records []struct {
 		Uuid         string `db:"uuid"`
 		SessionToken string `db:"session_token"`
+		CsrfToken    string `db:"csrf_token"`
 		CreatedAt    string `db:"created_at"`
 		ExpiresAt    string `db:"expires_at"`
 	}
