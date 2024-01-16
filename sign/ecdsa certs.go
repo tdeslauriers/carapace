64847e1ea7f22ea420db5b3dd67ae50a932c6021
tdeslauriers <thomas.deslauriers@gmail.com>
2024-01-16 17:13:44 -0600

    udpate packages

---
+++ sign/ecdsa certs.go
@@ -0,0 +1,167 @@
+package sign
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+type CertRole int
+
+const (
+	CA CertRole = iota
+	Server
+	Client
+)
+
+type CertFields struct {
+	CertName     string
+	Organisation []string
+	CommonName   string // org + signature algo, leaf: domain
+	San          []string
+	SanIps       []net.IP
+	Role         CertRole
+	CaCertName   string
+}
+
+func (fields *CertFields) GenerateEcdsaCert() {
+
+	// priave key (golang struct w/ objects and methods)
+	certPriv, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		log.Panicf("failed to create %s private key: %v", fields.CertName, err)
+	}
+
+	// certTemplate
+	certTemplate := fields.buildTemplate()
+
+	// signing cert template
+	var parentTemplate x509.Certificate
+	var signingPriv *ecdsa.PrivateKey
+	if fields.Role == CA {
+		// CA is self-signed
+		parentTemplate = certTemplate
+		signingPriv = certPriv
+	} else {
+		// load ca cert
+
+		caCertPem, err := os.ReadFile(fmt.Sprintf("%s-cert.pem", fields.CaCertName))
+		if err != nil {
+			log.Panicf("unable to read %s-cert.pem file", fields.CaCertName)
+		}
+
+		// load ca key
+		caKeyPem, err := os.ReadFile(fmt.Sprintf("%s-key.pem", fields.CaCertName))
+		if err != nil {
+			log.Panicf("unable to read %s-key.pem file", fields.CaCertName)
+		}
+
+		// decode pems to der
+		caCertBlock, _ := pem.Decode(caCertPem)
+
+		if caCertBlock == nil || caCertBlock.Type != "CERTIFICATE" {
+			log.Panic("failed to decode ca cert pem")
+		}
+
+		caKeyBlock, _ := pem.Decode(caKeyPem)
+		if caKeyBlock == nil || caKeyBlock.Type != "EC PRIVATE KEY" {
+			log.Panic("failed to decode ca key pem")
+		}
+
+		// parse root ca values
+		caCert, _ := x509.ParseCertificate(caCertBlock.Bytes)
+		caPriv, _ := x509.ParseECPrivateKey(caKeyBlock.Bytes)
+
+		parentTemplate = *caCert
+		signingPriv = caPriv
+	}
+
+	// create the certificate
+	derBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, &parentTemplate, &certPriv.PublicKey, signingPriv)
+	if err != nil {
+		log.Panicf("failed to create DER for %s certificate: %v", fields.CertName, err)
+	}
+
+	// write cert to file
+	certOut, err := os.Create(fmt.Sprintf("%s-cert.pem", fields.CertName))
+	if err != nil {
+		log.Panicf("failed to create file %s-cert.pem: %v", fields.CertName, err)
+	}
+	defer certOut.Close()
+
+	pem.Encode(certOut, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: derBytes,
+	})
+	certOut.Close()
+
+	// write private key out to file
+	keyOut, err := os.Create(fmt.Sprintf("%s-key.pem", fields.CertName))
+	if err != nil {
+		log.Panicf("failed to create file %s-key.pem: %v", fields.CertName, err)
+	}
+	defer keyOut.Close()
+
+	key, err := x509.MarshalECPrivateKey(certPriv)
+	if err != nil {
+		log.Panicf("failed to marshal ecdsa private key for %s: %v", fields.CertName, err)
+	}
+
+	pem.Encode(keyOut, &pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: key,
+	})
+	keyOut.Close()
+}
+
+func (fields *CertFields) buildTemplate() x509.Certificate {
+
+	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	if err != nil {
+		log.Panicf("unable to generate serial number for %s certificate template: %v", fields.CertName, err)
+	}
+
+	// validity period:
+	notBefore := time.Now()
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: fields.Organisation,
+			CommonName:   fields.CommonName,
+		},
+		NotBefore:             notBefore,
+		BasicConstraintsValid: true,
+		DNSNames:              fields.San,
+		IPAddresses:           fields.SanIps,
+	}
+
+	switch fields.Role {
+	case CA:
+		template.KeyUsage = x509.KeyUsageCertSign | x509.KeyUsageCRLSign
+		template.ExtKeyUsage = nil // not needed for CA
+		template.NotAfter = notBefore.Add(365 * 24 * time.Hour)
+		template.IsCA = true
+	case Server:
+		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
+		template.NotAfter = notBefore.Add(90 * 24 * time.Hour)
+		template.IsCA = false
+	default: // client case
+		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth}
+		template.NotAfter = notBefore.Add(90 * 24 * time.Hour)
+		template.IsCA = false
+	}
+
+	return template
+}
---
+++ sign/ecdsa_certs_test.go
@@ -0,0 +1,63 @@
+package sign
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"net"
+	"os"
+	"testing"
+)
+
+func TestCertValidation(t *testing.T) {
+
+	// set up
+	ca := CertFields{
+		CertName:     "rootCA",
+		Organisation: []string{"Rebel Alliance"},
+		CommonName:   "RebelAlliance ECDSA-SHA256",
+		Role:         CA,
+	}
+	ca.GenerateEcdsaCert()
+
+	leaf := CertFields{
+		CertName:     "server",
+		Organisation: []string{"Rebel Alliance"},
+		CommonName:   "localhost",
+		San:          []string{"localhost"},
+		SanIps:       []net.IP{net.ParseIP("127.0.0.1")},
+		Role:         Server,
+		CaCertName:   ca.CertName,
+	}
+	leaf.GenerateEcdsaCert()
+
+	// read generated certs
+	caCertPem, _ := os.ReadFile(fmt.Sprintf("%s-cert.pem", ca.CertName))
+	caDer, _ := pem.Decode(caCertPem)
+	caCert, _ := x509.ParseCertificate(caDer.Bytes)
+
+	leafCertPem, _ := os.ReadFile(fmt.Sprintf("%s-cert.pem", leaf.CertName))
+	leafDer, _ := pem.Decode(leafCertPem)
+	leafCert, _ := x509.ParseCertificate(leafDer.Bytes)
+
+	// cert pool
+	roots := x509.NewCertPool()
+	roots.AddCert(caCert)
+
+	// verify server cert
+	opts := x509.VerifyOptions{
+		Roots: roots,
+	}
+
+	if _, err := leafCert.Verify(opts); err != nil {
+		t.Logf("failed to validate server cert: %v", err)
+		t.Fail()
+	}
+
+	// clean up
+	// os.Remove(fmt.Sprintf("%s-cert.pem", ca.CertName))
+	// os.Remove(fmt.Sprintf("%s-key.pem", ca.CertName))
+	// os.Remove(fmt.Sprintf("%s-cert.pem", leaf.CertName))
+	// os.Remove(fmt.Sprintf("%s-key.pem", leaf.CertName))
+
+}
