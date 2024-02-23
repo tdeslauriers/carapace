aceac38e19c40c96e081f605831620438ff93a2b
tdeslauriers <thomas.deslauriers@gmail.com>
2024-02-23 08:21:29 -0600

    update json tags

--- session/auth_server.go
+++ session/auth_server.go
@@ -100,17 +100,17 @@ type S2sClientData struct {
 }
 
 type UserAccountData struct {
-	Uuid           string `db:"uuid" json:"uuid"`
+	Uuid           string `db:"uuid" json:"uuid,omitempty"`
 	Username       string `db:"username" json:"username"`
 	UserIndex      string `db:"user_index" json:"user_index,omitempty"`
-	Password       string `db:"password" json:"password"`
+	Password       string `db:"password" json:"password,omitempty"`
 	Firstname      string `db:"firstname" json:"firstname"`
 	Lastname       string `db:"lastname" json:"lastname"`
 	Birthdate      string `db:"birthdate" json:"bithdate,omitempty"` // string because field encrypted in db
 	CreatedAt      string `db:"created_at" json:"created_at"`
-	Enabled        bool   `db:"enabled"  json:"enabled"`
-	AccountExpired bool   `db:"acccount_expired" json:"account_expired"`
-	AccountLocked  bool   `db:"account_locked" json:"account_locked"`
+	Enabled        bool   `db:"enabled"  json:"enabled,omitempty"`
+	AccountExpired bool   `db:"acccount_expired" json:"account_expired,omitempty"`
+	AccountLocked  bool   `db:"account_locked" json:"account_locked,omitempty"`
 }
 
 // maps db table data, not jwt string
