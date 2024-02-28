b4ff2f5803971fc08aa8b161db12593367cd6602
tdeslauriers <thomas.deslauriers@gmail.com>
2024-02-28 12:33:26 -0600

    json error

---
+++ errors/json_err.go
@@ -0,0 +1,28 @@
+package errors
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type ErrorHttp struct {
+	StatusCode int    `json:"code"`
+	Message    string `json:"message"`
+}
+
+func (e ErrorHttp) SendJsonErr(w http.ResponseWriter) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	jsonErr, err := json.Marshal(e)
+	if err != nil {
+		log.Printf("error marshalling http error response to json: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"code": 500, "message": "Internal Server Error"}`))
+		return
+	}
+
+	w.WriteHeader(e.StatusCode)
+	w.Write(jsonErr)
+}
