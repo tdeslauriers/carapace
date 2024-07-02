a9f8514b28ecf113dbc02053ad279211bb578091
tdeslauriers <thomas.deslauriers@gmail.com>
2024-07-02 07:13:36 -0500

    adding domain client struct

--- pkg/session/auth_server.go
+++ pkg/session/auth_server.go
@@ -194,6 +194,19 @@ type S2sClientData struct {
 	AccountLocked  bool   `db:"account_locked" json:"account_locked"`
 }
 
+// IdentityClient is a struct for identity client data, NOT the same as S2S client data:
+// https://deslauriers.com vs the s2s shaw service
+type IdentityClient struct {
+	Uuid          string          `db:"uuid" json:"uuid"`
+	ClientId      string          `db:"client_id" json:"client_id"`
+	ClientName    string          `db:"client_name" json:"client_name"`
+	Description   string          `db:"description" json:"description"`
+	CreatedAt     data.CustomTime `db:"created_at" json:"created_at"`
+	Enabled       bool            `db:"enabled" json:"enabled"`
+	ClientExpired bool            `db:"client_expired" json:"client_expired"`
+	ClientLocked  bool            `db:"client_locked" json:"client_locked"`
+}
+
 type UserAccountClientXref struct {
 	Id        int    `db:"id" json:"id"`
 	AccountId string `db:"account_uuid" json:"account_uuid"`
