ce7e588e29e71c13bf1a741ff798a5024130a8d2
tdeslauriers <thomas.deslauriers@gmail.com>
2024-07-09 07:18:44 -0500

    updating AuthCodeExcahnge validation

--- pkg/session/auth_server.go
+++ pkg/session/auth_server.go
@@ -116,13 +116,42 @@ const (
 	AuthCode ResponseType = "code"
 )
 
-type AuthCodeResponse struct {
+type AuthCodeExchange struct {
 	AuthCode     string       `json:"auth_code"`
 	ResponseType ResponseType `json:"response_type"`
 	State        string       `json:"state"`
 	Nonce        string       `json:"nonce"`
 	ClientId     string       `json:"client_id"`
 	Redirect     string       `json:"redirect"`
+}
+
+// light validation for auth code exchange
+func (cmd *AuthCodeExchange) ValidateCmd() error {
+	if validate.TooShort(cmd.AuthCode, 16) || validate.TooLong(cmd.AuthCode, 64) {
+		return fmt.Errorf("invalid auth code: must be between %d and %d characters", 16, 64)
+	}
+
+	if validate.TooShort(string(cmd.ResponseType), 4) || validate.TooLong(string(cmd.ResponseType), 8) {
+		return fmt.Errorf("invalid response type: must be between %d and %d characters", 4, 8)
+	}
+
+	if validate.TooShort(cmd.State, 16) || validate.TooLong(cmd.State, 64) {
+		return fmt.Errorf("invalid state: must be between %d and %d characters", 16, 64)
+	}
+
+	if validate.TooShort(cmd.Nonce, 16) || validate.TooLong(cmd.Nonce, 64) {
+		return fmt.Errorf("invalid nonce: must be between %d and %d characters", 16, 64)
+	}
+
+	if validate.TooShort(cmd.ClientId, 16) || validate.TooLong(cmd.ClientId, 64) {
+		return fmt.Errorf("invalid client id: must be between %d and %d characters", 16, 64)
+	}
+
+	if validate.TooShort(cmd.Redirect, 6) || validate.TooLong(cmd.Redirect, 2048) {
+		return fmt.Errorf("invalid redirect: must be between %d and %d characters", 16, 2048)
+	}
+
+	return nil
 }
 
 type AccessTokenResponse struct {
