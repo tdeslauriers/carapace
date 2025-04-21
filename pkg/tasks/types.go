8cd06ea45c93880e179c0aa8e6add4a2498fa961
tdeslauriers <thomas.deslauriers@gmail.com>
2025-04-21 05:07:34 -0500

    task query param validation

--- pkg/tasks/types.go
+++ pkg/tasks/types.go
@@ -246,5 +246,244 @@ type Template struct {
 	Slug        string          `json:"slug,omitempty"`
 	CreatedAt   data.CustomTime `json:"created_at"`
 	IsArchived  bool            `json:"is_archived"`
-	Assignees   []profile.User     `json:"assignees"`
+	Assignees   []profile.User  `json:"assignees"`
+}
+
+// Task is a struct that represents a task as in json
+// It is a composite object of fields from task and task template models.
+// It also includes a slice of assignees.
+type Task struct {
+	Id             string          `json:"id,omitempty"`    // Tasks record uuid
+	Name           string          `json:"name"`            // Task template name
+	Description    string          `json:"description"`     // Task template description
+	Cadence        Cadence         `json:"cadence"`         // Task template cadence
+	Category       Category        `json:"category"`        // Task template category
+	CreatedAt      data.CustomTime `json:"created_at"`      // Task record created at
+	IsComplete     bool            `json:"is_complete"`     // Task record field
+	IsSatisfactory bool            `json:"is_satisfactory"` // Task record field
+	IsProactive    bool            `json:"is_proactive"`    // Task record field
+	Slug           string          `json:"slug,omitempty"`  // Task record slug
+	IsArchived     bool            `json:"is_archived"`     // Task record field
+	Assignees      profile.User    `json:"assignees"`       // Task assignee via xref (only one person per task record)
+}
+
+// TaskQueryParams is a map of keys that represents the allowed query parameters for a task template.
+var TaskQueryParams map[string]struct{} = map[string]struct{}{
+	"view":            {},
+	"assignee":        {},
+	"name":            {},
+	"cadence":         {},
+	"category":        {},
+	"is_complete":     {},
+	"is_satisfactory": {},
+	"is_proactive":    {},
+	"is_archived":     {},
+}
+
+// TaskViews is a map of keys that represents the allowed views for a task template.
+var TaskViews map[string]struct{} = map[string]struct{}{
+	"today": {},
+}
+
+// AssigneeCodes is map of keys that represents the allowed assignee shorthand for a task template.
+// Note: assignee can also be a username or a user slug (for the allowance table).
+var AssigneeCodes map[string]struct{} = map[string]struct{}{
+	"me":  {},
+	"all": {},
+}
+
+// ValidateQueryParams is a function that validates the query parameters for a task template.
+// It is used to validate the query parameters passed in the request.
+func ValidateQueryParams(params map[string][]string) error {
+
+	// check if params exist: redundant check, but it is a good practice
+	if len(params) == 0 {
+		return fmt.Errorf("no query parameters provided")
+	}
+
+	// check if params are valid
+	for k := range params {
+		if _, ok := TaskQueryParams[k]; !ok {
+			return fmt.Errorf("invalid query parameter: %s", k)
+		}
+	}
+
+	// check if view exists, and if so, is it valid
+	if view, ok := params["view"]; ok {
+
+		// check if view is empty
+		if len(view) <= 0 {
+			return fmt.Errorf("view parameter is not allowed to be empty if submitted")
+		}
+
+		// check if slice has more than one value
+		if len(view) > 1 {
+			return fmt.Errorf("view parameter must be a single value")
+		}
+
+		// dont need to split the values to check for multiple values because
+		// no views will ever have commas in them, so it will fail the key check below anyway.
+
+		// check if view is valid
+		if _, ok := TaskViews[strings.TrimSpace(strings.ToLower(view[0]))]; !ok {
+			return fmt.Errorf("invalid view parameter: %s", view)
+		}
+	}
+
+	// check if assignee exists, and if so, is it valid
+	if assignee, ok := params["assignee"]; ok {
+
+		// loop the values, and seperate any multiple values into single values
+		var assigneeList []string
+		for _, a := range assignee {
+			assigneeList = append(assigneeList, strings.TrimSpace(strings.ToLower(a)))
+		}
+
+		for _, a := range assigneeList {
+			// check if assignee is empty
+			if len(a) <= 0 {
+				return fmt.Errorf("assignee parameter is not allowed to be empty if submitted")
+			}
+
+			// check if assignee is valid
+			_, ok := AssigneeCodes[a]
+			emailErr := validate.IsValidEmail(a)
+			if !ok && emailErr != nil && !validate.IsValidUuid(a) {
+				return fmt.Errorf("invalid assignee parameter: %s", a)
+			}
+		}
+	}
+
+	// check if name exists, and if so, is it valid
+	if name, ok := params["name"]; ok {
+		// check if name is empty
+		if len(name) <= 0 {
+			return fmt.Errorf("name parameter is not allowed to be empty if submitted")
+		}
+
+		if len(name) > 1 {
+
+			// seperate any multiple values into single values
+			var nameList []string
+			for _, n := range name {
+				nameList = append(nameList, strings.TrimSpace(strings.ToLower(n)))
+			}
+			// check if name is valid
+			for _, n := range nameList {
+				if len(n) < 2 || len(n) > 64 {
+					return fmt.Errorf("name parameter must be between 2 and 64 characters in length")
+				}
+
+				if err := validate.IsValidName(n); err != nil {
+					return fmt.Errorf("invalid name parameter: %v", err)
+				}
+			}
+		}
+	}
+
+	// check if cadence exists, and if so, is it valid
+	if cadence, ok := params["cadence"]; ok {
+		// check if cadence is empty
+		if len(cadence) <= 0 {
+			return fmt.Errorf("cadence parameter is not allowed to be empty if submitted")
+		}
+
+		// artificially declare cadence param(s) a Cadence type to use Cadence.IsValidCadence()
+		for _, c := range cadence {
+			test := Cadence(strings.TrimSpace(strings.ToUpper(c)))
+			if (&test).IsValidCadence() != nil {
+				return fmt.Errorf("invalid cadence parameter: %s", c)
+			}
+		}
+	}
+
+	// check if category exists, and if so, is it valid
+	if category, ok := params["category"]; ok {
+		// check if category is empty
+		if len(category) <= 0 {
+			return fmt.Errorf("category parameter is not allowed to be empty if submitted")
+		}
+
+		// artificially declare category param(s) a Category type to use Category.IsValidCategory()
+		for _, c := range category {
+			test := Category(strings.TrimSpace(strings.ToUpper(c)))
+			if (&test).IsValidCategory() != nil {
+				return fmt.Errorf("invalid category parameter: %s", c)
+			}
+		}
+	}
+
+	// check if is_complete exists, and if so, is it valid
+	if isComplete, ok := params["is_complete"]; ok {
+		// check if is_complete is empty
+		if len(isComplete) <= 0 {
+			return fmt.Errorf("is_complete parameter is not allowed to be empty if submitted")
+		}
+
+		// cannot be more than one value because that is the same as "all", or "any" ==> unnecessary param
+		if len(isComplete) > 1 {
+			return fmt.Errorf("is_complete parameter must be a single value")
+		}
+		// check if is_complete is a boolean
+		isComplete[0] = strings.TrimSpace(strings.ToLower(isComplete[0]))
+		if isComplete[0] != "true" && isComplete[0] != "false" {
+			return fmt.Errorf("invalid is_complete parameter: %s", isComplete[0])
+		}
+	}
+
+	// check if is_satisfactory exists, and if so, is it valid
+	if isSatisfactory, ok := params["is_satisfactory"]; ok {
+		// check if is_satisfactory is empty
+		if len(isSatisfactory) <= 0 {
+			return fmt.Errorf("is_satisfactory parameter is not allowed to be empty if submitted")
+		}
+
+		// cannot be more than one value because that is the same as "all", or "any" ==> unnecessary param
+		if len(isSatisfactory) > 1 {
+			return fmt.Errorf("is_satisfactory parameter must be a single value")
+		}
+		// check if is_satisfactory is a boolean
+		isSatisfactory[0] = strings.TrimSpace(strings.ToLower(isSatisfactory[0]))
+		if isSatisfactory[0] != "true" && isSatisfactory[0] != "false" {
+			return fmt.Errorf("invalid is_satisfactory parameter: %s", isSatisfactory[0])
+		}
+	}
+
+	// check if is_proactive exists, and if so, is it valid
+	if isProactive, ok := params["is_proactive"]; ok {
+		// check if is_proactive is empty
+		if len(isProactive) <= 0 {
+			return fmt.Errorf("is_proactive parameter is not allowed to be empty if submitted")
+		}
+
+		// cannot be more than one value because that is the same as "all", or "any" ==> unnecessary param
+		if len(isProactive) > 1 {
+			return fmt.Errorf("is_proactive parameter must be a single value")
+		}
+		// check if is_proactive is a boolean
+		isProactive[0] = strings.TrimSpace(strings.ToLower(isProactive[0]))
+		if isProactive[0] != "true" && isProactive[0] != "false" {
+			return fmt.Errorf("invalid is_proactive parameter: %s", isProactive[0])
+		}
+	}
+
+	// check if is_archived exists, and if so, is it valid
+	if isArchived, ok := params["is_archived"]; ok {
+		// check if is_archived is empty
+		if len(isArchived) <= 0 {
+			return fmt.Errorf("is_archived parameter is not allowed to be empty if submitted")
+		}
+
+		// cannot be more than one value because that is the same as "all", or "any" ==> unnecessary param
+		if len(isArchived) > 1 {
+			return fmt.Errorf("is_archived parameter must be a single value")
+		}
+		// check if is_archived is a boolean
+		isArchived[0] = strings.TrimSpace(strings.ToLower(isArchived[0]))
+		if isArchived[0] != "true" && isArchived[0] != "false" {
+			return fmt.Errorf("invalid is_archived parameter: %s", isArchived[0])
+		}
+	}
+
+	return nil
 }
